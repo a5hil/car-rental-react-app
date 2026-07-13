bae759f2322b7cd507904cbc581c4efc08eadcf9
a5hil <ashil555anto@gmail.com>
2026-07-13 20:07:55 +0530

    NavBar and Routing done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
+import AddCar from './Components/AddCar'
+import ViewCars from './Components/ViewCars'
 
 function App() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/add-car' element={<AddCar />}></Route>
+          <Route path='/view-cars' element={<ViewCars />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
